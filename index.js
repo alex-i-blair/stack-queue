53a9e81c9672d7b74709bf276920cbf4b7df26a3
Alex Blair <bclan11@gmail.com>
2022-04-05 11:25:22 -0700

    queue class finished

---
+++ index.js
@@ -0,0 +1,33 @@
+class Queue {
+  constructor() {
+    this.index = 0;
+    this.list = [];
+  }
+  enqueue(item) {
+    this.list.push(item);
+  }
+
+  dequeue() {
+    this.index++;
+    return this.list[this.index - 1] ?? null;
+  }
+
+  hasNext() {
+    if (this.list[this.index + 1]) return true;
+    return false;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
